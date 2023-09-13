29f5f56cc1be50b276aa443c6b25e1fb176ee530
Thomas Sievers <thomas@qpix.com.br>
2023-09-13 19:31:25 -0300

    utilizando laços

--- JSPaginas/main.js
+++ JSPaginas/main.js
@@ -1,5 +1,16 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play()
+const listaTeclas = document.querySelectorAll('.tecla')
+
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom
+for(let contador = 0; contador < listaTeclas.length; contador++){
+
+    const tecla = listaTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}` //template string
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+}
