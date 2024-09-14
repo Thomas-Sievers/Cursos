94d230f9eda0b9cbb569fb809c48283e8104a62f
thomas <thomas@qpix.com.br>
2024-09-14 20:02:29 -0300

    commit atrasado

--- curso_node/server&client/app.js
+++ curso_node/server&client/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 
 //express app
 const app = express();
+
+//connect with mongodb
+const dbURI = 'mongodb+srv://Zaptas:Zaptas22t22t@nodetuts.1dnjc.mongodb.net/?retryWrites=true&w=majority&appName=NodeTuts'
 
 //register view engine
 app.set('view engine', 'ejs');
@@ -10,17 +14,26 @@ app.set('view engine', 'ejs');
 //listen for request
 app.listen(3000)
 
+//middleware & static files
+app.use(express.static('public'))
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title : 'Yoshi finds eggs', snippet : 'Lorem ipsum dolor sit amet consectetur'},
+        {title : 'Mario finds stars', snippet : 'Lorem ipsum dolor sit amet consectetur'},
+        {title : 'How to defeat Bowser', snippet : 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', { title : 'Home', blogs : blogs });
 });
 
 app.get('/about', (req, res) => {
     //res.send('<p>About page</p>');
-    res.render('about')
+    res.render('about', { title: 'About'})
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create')
+    res.render('create', { title: 'create a new blog'})
 })
 
 //redirects
@@ -30,5 +43,5 @@ app.get('/about-us', (req, res) => {
 
 //404 page // sempre última rota
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404'});
 })
