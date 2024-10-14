bd65f85a230d86549a978481d2000d9b589b514d
thomas <thomas@qpix.com.br>
2024-10-14 09:40:35 -0300

    fazendo o aside e os cards de anotação

--- react_node/Frontend/src/App.jsx
+++ react_node/Frontend/src/App.jsx
@@ -1,27 +1,38 @@
 import { useState } from 'react'
 import Header from './Componentes/Header'
+import Notes from './Componentes/Notes'
 
-// Componente = é uma estrutura do código que retorna um HTML, CSS ou JS
-// Propriedades = São informações que um componente Pai, passa pra um componente filho
-// Estado = função que manipula uma informaçõa e armazena a mesma
+import './styles/global.css'
+import './styles/sidebar.css'
+import './styles/app.css'
+import './styles/main.css'
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  function handleSoma(){
-    setCount(count + 1);
-  }
-
-  function handleMenos(){
-    setCount(count - 1);
-  }
-
+function App(){
   return (
-    <>
-      <h1>{count}</h1>
-      <button onClick={handleSoma}>+</button>
-      <button onClick={handleMenos}>-</button>
-    </>
+    <div id='app'>
+      <aside>
+        <strong>Caderno de notas</strong>
+
+        <form>
+          <div className='input-block'>
+            <label htmlFor="title">Títula da anotação</label>
+            <input/>
+          </div>
+        </form>
+
+        <div className="input-block">
+          <label htmlFor="nota">Anotações</label>
+          <textarea></textarea>
+        </div>
+
+        <button type="submit">Salvar</button>
+      </aside>
+      <main>
+        <ul>
+          <Notes />
+        </ul>
+      </main>
+    </div>
   )
 }
 
---
+++ react_node/Frontend/src/Componentes/Notes.jsx
@@ -0,0 +1,77 @@
+export default function Notes(){
+    return(
+        <>
+        <li className="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+
+            <textarea>lorem ipsum e nao sei qo sei lao oq</textarea>
+            <span>!</span>
+        </li>
+
+        <li className="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+
+            <textarea>lorem ipsum e nao sei qo sei lao oq</textarea>
+            <span>!</span>
+        </li>
+
+        <li className="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+
+            <textarea>lorem ipsum e nao sei qo sei lao oq</textarea>
+            <span>!</span>
+        </li>
+
+        <li className="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+
+            <textarea>lorem ipsum e nao sei qo sei lao oq</textarea>
+            <span>!</span>
+        </li>
+
+        <li className="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+
+            <textarea>lorem ipsum e nao sei qo sei lao oq</textarea>
+            <span>!</span>
+        </li>
+
+        <li className="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+
+            <textarea>lorem ipsum e nao sei qo sei lao oq</textarea>
+            <span>!</span>
+        </li>
+        </>
+    )
+};
