683986481a03fd49c2338f67a66c7c9a0e2918bc
thomas <thomas@qpix.com>
2025-03-17 11:26:32 -0300

    conceitos básicos de java

---
+++ curso_java/screenMatch/src/Film.java
@@ -0,0 +1,8 @@
+public class Film {
+    String name;
+    int releaseYear;
+    boolean includedPlan;
+    double evaluation;
+    int evaluationTotal;
+    int duration;
+}
---
+++ curso_java/screenMatch/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Film myFilm = new Film();
+        myFilm.name = "Poderoso Chefão";
+        myFilm.releaseYear = 1970;
+        myFilm.duration = 180;
+
+        System.out.println(myFilm.name);
+        System.out.println(myFilm.releaseYear);
+    }
+}
